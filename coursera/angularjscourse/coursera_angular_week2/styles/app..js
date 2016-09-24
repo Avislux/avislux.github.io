7760f9ceddc6dcfb3278fb4be6e09f170d6c63e2
Avislux <gameguy94@hotmail.com>
2016-09-23 20:23:43 -0700

    refactoring. angularjscourse update

---
+++ coursera/angularjscourse/coursera_angular_week2/styles/app..js
@@ -0,0 +1,10 @@
+/**
+ * Created by Jonathan on 9/21/2016.
+ */
+(function(){
+'use strict';
+angular.module('ShoppingListCheckoff', [])
+
+.controller('LunchCheckController',LunchCheckController);
+
+})();
